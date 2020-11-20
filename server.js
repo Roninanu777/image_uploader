37c58e298ac85812d09d871a5e9e902f8275a44d
Roni Raj Kamal Pradhan <ronirajkamalpradhan1112@gmail.com>
2020-11-21 01:22:33 +0530

    entry file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
-
+const imageRouter = require("./routes/imageRouter");
 const app = express();
 
 app.use(express.json());
@@ -14,6 +14,8 @@ mongoose
     useFindAndModify: true,
   })
   .then(() => console.log("MongoDB connected..."));
+
+app.use("/", imageRouter);
 
 const PORT = process.env.PORT || 5000;
 
