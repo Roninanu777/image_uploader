e052d16635a8907cc56a3231015c6d2f745ed2cd
Roni Raj Kamal Pradhan <ronirajkamalpradhan1112@gmail.com>
2020-11-21 01:22:09 +0530

    defining routers

--- routes/imageRouter.js
+++ routes/imageRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
-const imageModel = require("../models/Image");
+const ImageModel = require("../models/Image");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -15,3 +15,32 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage: storage });
+
+router.post("/", upload.single("image"), (req, res, next) => {
+  const obj = {
+    name: req.body.name,
+    desc: req.body.desc,
+    img: {
+      data: fs.readFileSync(
+        path.join(__dirname + "/uploads", req.file.filename)
+      ),
+      contentType: "image/png",
+    },
+  };
+
+  ImageModel.create(obj, (err, item) => {
+    if (err) console.log(err);
+    else {
+      res.redirect("/");
+    }
+  });
+});
+
+router.get("/", (req, res) => {
+  ImageModel.find({}, (err, items) => {
+    if (err) console.log(err);
+    else {
+      res.status(200).json({ items: items });
+    }
+  });
+});
