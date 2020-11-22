fbaf3956c2bf56bb944e277fd0db71b3752e0700
Roni Raj Kamal Pradhan <ronirajkamalpradhan1112@gmail.com>
2020-11-22 21:25:21 +0530

    adding cors

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const imageRouter = require("./routes/imageRouter");
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
